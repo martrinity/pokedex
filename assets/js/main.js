359fe0739d28e7f78d1dd77d516e51c61321c189
martrinity <martiloca_4@hotmail.com>
2017-07-07 15:41:16 -0400

    mostrando pokemon, usando sprite de url externa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var dibujarGifs = function(data){
+	/*var dibujarPokes = function(data){
 		var poke = "";
 		var url = "";
 		data.forEach(function(element){
@@ -12,30 +12,36 @@ $(document).ready(function() {
 	var armarTemplate = function(poke,url){
 		var t = "<div class='elemento'><img src='" + poke + "'/><a href='" + url +"'>Ver más</a></div>"
 		return t;
-	}
+	}*/
 
-	var ajaxpoke = function(poke){
+	var ajaxPoke = function(poke){
 		$.ajax({
-			url : 'http://pokeapi.co/api/v2/pokedex/1/', 
+			url : 'http://pokeapi.co/api/v2/pokemon/', 
 			type : 'GET',
 			datatype : 'json',
-			data : {
-				q : poke,
-				
-			}
+			data : {'limit' : '780'}
 		})
+
 		.done(function(response){
-			console.log(response);
-			dibujarpokes(response.data);
+			response.results.forEach(function(el){
+				$("#elementos").append("<div class='col-lg-3 tarjetas'><img src='https://img.pokemondb.net/sprites/x-y/normal/"+ el.name +".png' alt=‘"+el.name+"'><h5>"+el.name+"</h5></div>");
+
+			})
+			console.log('response');
+	
 		})
+
 		.fail(function(){
 			console.log("error");
 		});
+
+		
 	}
 
-	$("#buscar").click(function(event) {
+	/*$("#buscar").click(function(event) {
 		console.log("Entro");
 		var poke = $("#poke-text").val();
 		ajaxpoke(poke);
-	});
+	});*/
+	ajaxPoke();
 });
